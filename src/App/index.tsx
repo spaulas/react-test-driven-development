38a0d0a8f7c0f5efbe1a19362665334e919433f0
pikaula <s.paula@ua.pt>
2021-02-04 06:51:01 +0000

    feat(project-init): change main file types

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Appointments  from "@pages/Appointments";
+import "./App.scss";
 
 function App(): JSX.Element {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Appointments />
+    </div>
+  );
 }
 
 export default App;
