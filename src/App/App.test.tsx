ebbc8fbbc6e77a3de2ffd1f0af50596193e83407
pikaula <s.paula@ua.pt>
2021-02-06 10:46:31 +0000

    feat(customer-form): add router

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from ".";
 
+// TODO create test for routes
 test("renders learn react link", () => {
   render(<App />);
   expect(true).toBe(true);
--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,11 +1,30 @@
 import React from "react";
-import Appointments  from "@pages/Appointments";
+import Appointments from "@pages/Appointments";
+import Home from "@pages/Home";
+import AddCustomer from "@pages/AddCustomer";
+import AddAppointment from "@pages/AddAppointment";
 import "./App.scss";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App(): JSX.Element {
   return (
     <div className="app">
-      <Appointments />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/appointments">
+            <Appointments />
+          </Route>
+          <Route exact path="/add-customer">
+            <AddCustomer />
+          </Route>
+          <Route exact path="/add-appointment">
+            <AddAppointment />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
